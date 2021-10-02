1d3f2d1d4cce775d6bce9a614523f92900046e36
Art <ya.Artriches@ya.ru>
2021-10-02 14:16:07 +0300

    feat: Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import { Header, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
+import { Title, Rating } from '../components';
 
-export default function Home(): JSX.Element {
+export default withLayout(function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <div>
-      <Header tag="h1">Rating</Header>
+    <>
+      <Title tag="h1">Rating</Title>
 
       <Rating isEditable rating={rating} setRating={setRating} />
-    </div>
+    </>
   );
-}
+});
